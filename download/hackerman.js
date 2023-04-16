a01c23ba9020a76468e150ce4e79a4968588dfa9
Pranav Ramesh <laphatize@protonmail.com>
2023-04-16 12:45:48 -0400

    Create hackerman.js

---
+++ download/hackerman.js
@@ -0,0 +1,5 @@
+var colors = ["red", "blue", "green", "yellow", "orange", "purple"];
+var color = colors[Math.floor(Math.random() * colors.length)];
+console.log("I spy something " + color);
+var key = colors[1] + colors[2] + 21;
+// console.log(key)
